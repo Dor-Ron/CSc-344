90ce156c153a570dca97bf01a2b9f8da243f6d8c
Dor Rondel <dor.rondel@gmail.com>
2017-02-02 14:46:58 -0500

    initial commit

---
+++ 0_1/filecomplete.c
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <dirent.h> // necessary for directory analysis
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char folder_name[100];
+	printf("Enter file directory: \t");
+	scanf("%[^\n]s", folder_name);
+	printf("%s", folder_name);
+	DIR *dir;
+	struct dirent *files;
+	if ((dir = opendir(folder_name)) != NULL) {
+  	while ((files = readdir (dir)) != NULL) { // print each file in directory
+    		printf("%s\n", files->d_name);
+  		}
+  		closedir (dir);
+		} else { // problem opening directory
+  		perror("");
+	  	return EXIT_FAILURE;
+	}
+	return 0;
+}
