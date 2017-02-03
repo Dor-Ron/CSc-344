2f06669a91c62d3fef88436f65bce54d39ec0020
Dor Rondel <dor.rondel@gmail.com>
2017-02-03 15:53:57 -0500

    fixed spacing issue

--- 0_1/filecomplete.c
+++ 0_1/filecomplete.c
@@ -12,30 +12,30 @@ bool contains(const char *a, const char *b) {
 }
 
 int main() {
-	char folderName[100];
+  char folderName[100];
   char fileStart[100];
   const char *fileArray[100];
   int idx = 0;
-	printf("Enter a folder name: \t");
-	scanf("%[^\n]s", folderName);
+  printf("Enter a folder name: \t");
+  scanf("%[^\n]s", folderName);
   fseek(stdin, 0, SEEK_END);
-	printf("Enter the beginning of a filename: ");
+  printf("Enter the beginning of a filename: ");
   scanf("%[^\n]s", fileStart);
   fseek(stdin, 0, SEEK_END);
-	DIR *dir;
-	struct dirent *files;
-	if ((dir = opendir(folderName)) != NULL) {     // if pointer to directory exists
-  	while ((files = readdir (dir)) != NULL) {    // if there are still files to go through in directory
+  DIR *dir;
+  struct dirent *files;
+  if ((dir = opendir(folderName)) != NULL) {     // if pointer to directory exists
+    while ((files = readdir (dir)) != NULL) {    // if there are still files to go through in directory
       if (contains(files->d_name, fileStart)) {  // if files start with substring
-          fileArray[idx] = files->d_name;        // add file to array
-          idx++;
+        fileArray[idx] = files->d_name;        // add file to array
+        idx++;
       }
-  	}
+    }
     closedir(dir);
     for (int i = 0; i < idx; i++) printf("%s\n", fileArray[i]);
-	} else { // problem opening directory
-  		perror("");
-	  	return EXIT_FAILURE;
-	}
-	return 0;
+  } else { // problem opening directory
+      perror("");
+      return EXIT_FAILURE;
+  }
+  return 0;
 }
