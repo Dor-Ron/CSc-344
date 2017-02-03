abf9ad04f981079d014dea9fade84712f5a10a1a
Dor Rondel <dor.rondel@gmail.com>
2017-02-03 15:49:40 -0500

    updated file, can search and find files now

--- 0_1/filecomplete.c
+++ 0_1/filecomplete.c
@@ -3,19 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+/* Check if string starts with substring */
+bool contains(const char *a, const char *b) {
+   if(strncmp(a, b, strlen(b)) == 0) return 1;
+   return 0;
+}
 
 int main() {
-	char folder_name[100];
-	printf("Enter file directory: \t");
-	scanf("%[^\n]s", folder_name);
-	printf("%s", folder_name);
+	char folderName[100];
+  char fileStart[100];
+  const char *fileArray[100];
+  int idx = 0;
+	printf("Enter a folder name: \t");
+	scanf("%[^\n]s", folderName);
+  fseek(stdin, 0, SEEK_END);
+	printf("Enter the beginning of a filename: ");
+  scanf("%[^\n]s", fileStart);
+  fseek(stdin, 0, SEEK_END);
 	DIR *dir;
 	struct dirent *files;
-	if ((dir = opendir(folder_name)) != NULL) { // if pointer to directory exists
-  	while ((files = readdir (dir)) != NULL) { // print each file in directory while pointer exists to file
-    		printf("%s\n", files->d_name);
-  		} closedir(dir);
-		} else { // problem opening directory
+	if ((dir = opendir(folderName)) != NULL) {     // if pointer to directory exists
+  	while ((files = readdir (dir)) != NULL) {    // if there are still files to go through in directory
+      if (contains(files->d_name, fileStart)) {  // if files start with substring
+          fileArray[idx] = files->d_name;        // add file to array
+          idx++;
+      }
+  	}
+    closedir(dir);
+    for (int i = 0; i < idx; i++) printf("%s\n", fileArray[i]);
+	} else { // problem opening directory
   		perror("");
 	  	return EXIT_FAILURE;
 	}
