967a337ee0e96d9c6bbda05b6f52a9bc562993d3
Dor Rondel <dor.rondel@gmail.com>
2017-02-13 19:38:07 -0500

    finally finished :D

--- 0_1/filecomplete.c
+++ 0_1/filecomplete.c
@@ -2,7 +2,7 @@
    Course: CSc 344 - Programming Languages
    Assignment #: 1
    Instructor: Professor Daniel Schlegel
-   Authors: [
+   Author: [
       Dor Rondel,
       Nirender
    ]
@@ -28,8 +28,8 @@ struct node {
 /* Add nodes to linked list */
 void addNode(char *data, struct node **node_ptr) {
    struct node *tmp_ptr;
-   if (*node_ptr == NULL) {
-      *node_ptr = (struct node *) malloc(sizeof(struct node)); // first link
+   if (*node_ptr == NULL) { // first link
+      *node_ptr = (struct node *) malloc(sizeof(struct node));
       (*node_ptr)->data = data;
       (*node_ptr)->next = NULL;
    }
@@ -45,12 +45,12 @@ void addNode(char *data, struct node **node_ptr) {
 /* traverse linked list and print the corresponding file names */
 void printFiles(struct node *head_ptr, char *dirName, char *startString) {
    printf("\nFiles beginning with %s in %s:\n", startString, dirName);
-   for (struct node *i = head_ptr; i != NULL ; i = i->next) printf("%s\n", i->data);
+   for (struct node *i = head_ptr; i != NULL; i = i->next) printf("%s\n", i->data);
 }
 
 /* recursively clear linked list and avoid dangling pointers */
 void clearList(struct node **head_ptr) {
-  for (struct node *i = *head_ptr; i != NULL ; i = i->next) {
+  for (struct node *i = *head_ptr; i != NULL; i = i->next) {
     if (i->next != NULL) {
       clearList(&(i->next));
       i->next = NULL;
@@ -60,19 +60,19 @@ void clearList(struct node **head_ptr) {
 }
 
 /* clear array entries */
-void clearArray(struct node *arr[26]) {
-  for (int i = 0; i < 25; i++) {
+void clearArray(struct node *arr[64]) {
+  for (int i = 0; i < 64; i++) {
     if (arr[i] != NULL) clearList(&arr[i]);
   }
 }
 
 /* Make array of length 26 with each index representing a letter set to NULL */
-void makeLetterArray(struct node **arr[26]) {
-  for (int i = 0; i < 25; i++) (*arr)[i] = NULL;
+void makeLetterArray(struct node **arr[64]) {
+  for (int i = 0; i < 64; i++) (*arr)[i] = NULL;
 }
 
 /* reset data for when user enters new directory */
-void resetData(struct node *arr[26]) {
+void resetData(struct node *arr[64]) {
   makeLetterArray(&arr);
   clearArray(arr);
 }
@@ -83,18 +83,25 @@ void resetData(struct node *arr[26]) {
 
 /* Return index by subtracting ASCII value of first char in d_name string */
 int letterIndex(const char *file) {
-  return ((int)file[0]) - 97;
+  int charAscii = (int)file[0];
+  // if valid input, file beginning with a-zA-Z0-9 or .
+  if (charAscii == 46 || (charAscii >= 48 && charAscii <= 57) || (charAscii >= 65 && charAscii <= 90) || (charAscii >= 97 && charAscii <= 122)) {
+    if (charAscii == 46) return 62;  // char is a .
+    if (charAscii >= 48 && charAscii <= 57) return charAscii + 4; // char is a number
+    if (charAscii >= 65 && charAscii <= 90) return charAscii - 39; // capital letter
+    return charAscii - 97; // lower case letter
+  }
+  return 54321; // character not supported
 }
 
 /* Check if string starts with substring */
-bool contains(const char *full, const char *start) {
+bool begins(const char *full, const char *start) {
    if(strncmp(full, start, strlen(start)) == 0) return true;
    return false;
 }
 
 int main() {
-  struct node *linkedListArray[26];
-  struct node *tmp_head;
+  struct node *linkedListArray[64];
   char folderName[100];
   char fileStart[100];
   int letterIdx = 0;
@@ -106,17 +113,19 @@ int main() {
     printf("Enter the beginning of a filename: ");
     scanf("%[^\n]s", fileStart);
     fseek(stdin, 0, SEEK_END);
-    if (!folderName[0]) break;
+    if (!fileStart[0]) break;
     DIR *dir;
     struct dirent *files;
     letterIdx = letterIndex(fileStart);
     if ((dir = opendir(folderName)) != NULL) {     // if pointer to directory exists
       while ((files = readdir(dir)) != NULL) {    // if there are still files to go through in directory
-        if (letterIndex(files->d_name) > 0) addNode(files->d_name, &linkedListArray[letterIndex(files->d_name)]);
+        if (letterIndex(files->d_name) >= 0 && letterIndex(files->d_name) <= 62) {  // valid first character
+         addNode(files->d_name, &linkedListArray[letterIndex(files->d_name)]);
+        }
+        if (begins(files->d_name, fileStart)) addNode(files->d_name, &linkedListArray[63]); // match with user input
       }
       closedir(dir);
-      tmp_head = linkedListArray[letterIdx];
-      printFiles(tmp_head, folderName, fileStart);
+      printFiles(linkedListArray[63], folderName, fileStart);
     } else perror("No such directory exists, please enter a valid path:\n"); // problem opening directory
     memset(folderName, 0, strlen(folderName));
     memset(fileStart, 0, strlen(fileStart));
