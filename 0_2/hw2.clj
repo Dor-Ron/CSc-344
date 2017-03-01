1f4bd05b29fb8d0c4509d1006eb54790977309ac
Dor Rondel <dor.rondel@gmail.com>
2017-03-01 00:01:39 -0500

    added hashmap supplied by the professor

--- 0_2/hw2.clj
+++ 0_2/hw2.clj
@@ -13,3 +13,24 @@
   (reduce
     (fn [e1 e2]
       (list 'and e1 e2)) args))
+
+;Boolean Algebra Simplication HashMap.class Supplied
+(def boolExpMap
+  (hash-map '(or true) true,
+    '(or false) false,
+    '(and true) true,
+    '(and false) false,
+    '(or x false) 'x,
+    '(or false x) 'x,
+    '(or true x) true,
+    '(or x true) true,
+    '(and x false) false,
+    '(and false x) false,
+    '(and x true) 'x,
+    '(and true x) 'x,
+    '(not false) true,
+    '(not true) false,
+    '(or x y z) '(or x (or y z)),
+    '(and x y z) '(and x (and y z)),
+    '(not (and x y)) '(or (not x) (not y)),
+    '(not (or x y)) '(and (not x) (not y))))
