b4061da7ca6f3492ad5c7deafe22dc1020ba9fbc
Dor Rondel <dor.rondel@gmail.com>
2017-03-08 09:11:50 -0500

    added progress

--- 0_2/hw2.clj
+++ 0_2/hw2.clj
@@ -6,7 +6,7 @@
 ; Supplied to us by Prof. Schlegel
 (defn notexp [e1] (list 'not e1))
 
-;Redefined orexp and andexp to take variable # of parameters
+; Redefined orexp and andexp to take variable # of parameters
 (defn orexp [& args]
   (reduce
     (fn [e1 e2]
@@ -17,23 +17,40 @@
     (fn [e1 e2]
       (list 'and e1 e2)) args))
 
-;Boolean Algebra Simplication HashMap.class Supplied
-(def boolExpMap
-  (hash-map '(or true) true,
-    '(or false) false,
-    '(and true) true,
-    '(and false) false,
-    '(or x false) 'x,
-    '(or false x) 'x,
-    '(or true x) true,
-    '(or x true) true,
-    '(and x false) false,
-    '(and false x) false,
-    '(and x true) 'x,
-    '(and true x) 'x,
-    '(not false) true,
-    '(not true) false,
-    '(or x y z) '(or x (or y z)),
-    '(and x y z) '(and x (and y z)),
-    '(not (and x y)) '(or (not x) (not y)),
-    '(not (or x y)) '(and (not x) (not y))))
+
+;Binding provided by Prof. Schlegel
+(defn bind-values [l m]
+  (map #(cond
+          (seq? %) (bind-values % m)
+          :default (m % %))
+       l))
+
+;Substitute expressions for simpler forms cases
+(defn subExpr [lst]
+  (let [[op & args][(first lst) (rest lst)]]
+      (cond
+        (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(true))) true        ;(or true)
+        (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(false))) false      ;(or false)
+        (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(true))) true       ;(and true)
+        (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(false))) false     ;(and false)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (first (first args)) true)) (= (second (first args)) false))))) (first (first args))      ;(or <const> false)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (second (first args)) true)) (= (first (first args)) false))))) (second (first args))     ;(or <const> false)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true))))) true                     ;(or true <const>)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true))))) true                    ;(or <const> true)
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) false))))) false                 ;(and <const> false)
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) false))))) false                  ;(and false <const>)
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true))))) (first (first args))   ;(and <const> true)
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true))))) (second (first args))   ;(and true <const>)
+        (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(true))) false      ;(not true)
+        (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(false))) true      ;(not false)
+        (and (= op 'or) (= (count (first args)) 3)) (orexp (second (first args)) (nth (first args) 2) (first (first args)))    ;(or x y z)
+        (and (= op 'and) (= (count (first args)) 3)) (andexp (second (first args)) (nth (first args) 2) (first (first args)))  ;(and x y z)
+        (and (= op 'not) (= (first (first (first args))) 'and)) (orexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (and x y))
+        (and (= op 'not) (= (first (first (first args))) 'or)) (andexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (or x y))
+        ;---- Own Simplifications
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) false            ;(and true false)
+        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) false            ;(and false true)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) true              ;(and true false)
+        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) true              ;(and false true)
+        :else lst
+    )))
