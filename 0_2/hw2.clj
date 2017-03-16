13a8f0e2435d201fc95ba384d3b6e0bb00908e42
Dor Rondel <dor.rondel@gmail.com>
2017-03-16 04:25:17 -0400

    finished #2 started #3

--- 0_2/hw2.clj
+++ 0_2/hw2.clj
@@ -1,5 +1,8 @@
 ;File: hw2.clj
 ;Authors: Dor Rondel and Nirender
+;Instructor: Prof. Daniel Schlegel
+;Course: CSc 344
+;Institution: SUNY Oswego 
 
 (ns hw2)
 
@@ -25,35 +28,58 @@
           :default (m % %))
        l))
 
+; make boolean checker function
+; Built-in to clojure >1.9 but for backward compatability purposes..
+(defn boolean? [x]
+ (instance? Boolean x))
+
 ;Substitute expressions for simpler forms cases
 (defn subExpr [lst]
-  (let [[op & args][(first lst) (rest lst)]]
-      (cond
-        (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(true))) true        ;(or true)
-        (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(false))) false      ;(or false)
-        (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(true))) true       ;(and true)
-        (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(false))) false     ;(and false)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (first (first args)) true)) (= (second (first args)) false))))) (first (first args))      ;(or <const> false)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (second (first args)) true)) (= (first (first args)) false))))) (second (first args))     ;(or <const> false)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true))))) true                     ;(or true <const>)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true))))) true                    ;(or <const> true)
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) false))))) false                 ;(and <const> false)
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) false))))) false                  ;(and false <const>)
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true))))) (first (first args))   ;(and <const> true)
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true))))) (second (first args))   ;(and true <const>)
-        (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(true))) false      ;(not true)
-        (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(false))) true      ;(not false)
-        (and (= op 'or) (= (count (first args)) 3)) (orexp (second (first args)) (nth (first args) 2) (first (first args)))    ;(or x y z)
-        (and (= op 'and) (= (count (first args)) 3)) (andexp (second (first args)) (nth (first args) 2) (first (first args)))  ;(and x y z)
-        (and (= op 'not) (= (first (first (first args))) 'and)) (orexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (and x y))
-        (and (= op 'not) (= (first (first (first args))) 'or)) (andexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (or x y))
-        ;---- Own Simplifications
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) false            ;(and true false)
-        (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) false            ;(and false true)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) true              ;(and true false)
-        (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) true              ;(and false true)
-        :else lst
-    )))
+  (println "\nGoing back up recursively")
+  (println lst)
+  (if (or (boolean? lst) (symbol? lst)) lst
+    (let [[op & args][(first lst) (rest lst)]]
+        (cond
+          (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(true))) true        ;(or true)
+          (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(false))) false      ;(or false)
+          (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(true))) true       ;(and true)
+          (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(false))) false     ;(and false)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (first (first args)) true)) (= (second (first args)) false))))) (first (first args))      ;(or <const> false)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (second (first args)) true)) (= (first (first args)) false))))) (second (first args))     ;(or <const> false)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true))))) true                       ;(or true <const>)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true))))) true                      ;(or <const> true)
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) false))))) false                   ;(and <const> false)
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) false))))) false                    ;(and false <const>)
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true))))) (first (first args))     ;(and <const> true)
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true))))) (second (first args))     ;(and true <const>)
+          (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(true))) false      ;(not true)
+          (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(false))) true      ;(not false)
+          (and (= op 'or) (= (count (first args)) 3)) (orexp (second (first args)) (nth (first args) 2) (first (first args)))    ;(or x y z)
+          (and (= op 'and) (= (count (first args)) 3)) (andexp (second (first args)) (nth (first args) 2) (first (first args)))  ;(and x y z)
+          (and (= op 'not) (= (first (first (first args))) 'and)) (orexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (and x y))
+          (and (= op 'not) (= (first (first (first args))) 'or)) (andexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (or x y))
+          ;---- Own Simplifications
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) false            ;(and true false)
+          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) false            ;(and false true)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) true              ;(and true false)
+          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) true              ;(and false true)
+          :else lst
+      ))))
 
-; need to write this one...
-(defn simplify [])
+; Recursively applies subExpr to unevalueted boolean expression list
+(defn simplify [lst]
+    (println "Going Down Recursively:\n***")
+    (println lst)
+      (cond
+        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (not (seq? (second lst)))) (not (seq? (nth lst 2)))) (subExpr lst)
+        (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 2)) (not (seq? (second lst)))) (subExpr lst)
+        (and (and (and (not (boolean? lst)) (seq? (first lst))) (= (count lst) 2)) (not (seq? (second lst)))) (subExpr (list (second lst) (simplify (first lst))))
+        (and (and (and (not (boolean? lst)) (seq? (second lst))) (= (count lst) 2)) (not (seq? (first lst)))) (subExpr (list (first lst) (simplify (second lst))))
+        (and (and (and (and (not (boolean? lst)) (seq? (first lst))) (and (= (count lst) 3))) (not (seq? (second lst)))) (not (seq? (nth lst 2)))) (subExpr (list (second lst) (nth lst 2) (simplify (first lst))))
+        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (seq? (second lst))) (not (seq? (nth lst 2)))) (subExpr (list (first lst) (simplify (second lst)) (nth lst 2)))
+        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (not (seq? (second lst)))) (seq? (nth lst 2))) (subExpr (list (first lst) (second lst) (simplify (nth lst 2))))
+        :else lst
+  ))
+
+; Main function
+(defn evalexp [exp bindings] (simplify (bind-values bindings exp)))
---
+++ 0_3/proof_checker.hs
@@ -0,0 +1,9 @@
+{-
+File: proof_checker.hs
+Authors:
+  Dor Rondel
+  Nirender
+Instructor: Prof. Daniel Schlegel
+Course: CSc 344
+Institution: SUNY Oswego
+-}
