a289b470eb66ef6864c751cbe47a3c028e10585f
Dor Rondel <dor.rondel@gmail.com>
2017-03-21 22:26:08 -0400

    cleaned up code, added some functionality to simplify

--- 0_2/hw2.clj
+++ 0_2/hw2.clj
@@ -2,7 +2,7 @@
 ;Authors: Dor Rondel and Nirender
 ;Instructor: Prof. Daniel Schlegel
 ;Course: CSc 344
-;Institution: SUNY Oswego 
+;Institution: SUNY Oswego
 
 (ns hw2)
 
@@ -14,6 +14,7 @@
   (reduce
     (fn [e1 e2]
       (list 'or e1 e2)) args))
+
 
 (defn andexp [& args]
   (reduce
@@ -31,7 +32,7 @@
 ; make boolean checker function
 ; Built-in to clojure >1.9 but for backward compatability purposes..
 (defn boolean? [x]
- (instance? Boolean x))
+  (instance? Boolean x))
 
 ;Substitute expressions for simpler forms cases
 (defn subExpr [lst]
@@ -40,44 +41,261 @@
   (if (or (boolean? lst) (symbol? lst)) lst
     (let [[op & args][(first lst) (rest lst)]]
         (cond
-          (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(true))) true        ;(or true)
-          (and (and (= op 'or) (= (count (first args)) 1)) (= (first args) '(false))) false      ;(or false)
-          (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(true))) true       ;(and true)
-          (and (and (= op 'and) (= (count (first args)) 1)) (= (first args) '(false))) false     ;(and false)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (first (first args)) true)) (= (second (first args)) false))))) (first (first args))      ;(or <const> false)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (not (= (second (first args)) true)) (= (first (first args)) false))))) (second (first args))     ;(or <const> false)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true))))) true                       ;(or true <const>)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true))))) true                      ;(or <const> true)
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) false))))) false                   ;(and <const> false)
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) false))))) false                    ;(and false <const>)
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true))))) (first (first args))     ;(and <const> true)
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true))))) (second (first args))     ;(and true <const>)
-          (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(true))) false      ;(not true)
-          (and (and (= op 'not) (= (count (first args)) 1)) (= (first args) '(false))) true      ;(not false)
-          (and (= op 'or) (= (count (first args)) 3)) (orexp (second (first args)) (nth (first args) 2) (first (first args)))    ;(or x y z)
-          (and (= op 'and) (= (count (first args)) 3)) (andexp (second (first args)) (nth (first args) 2) (first (first args)))  ;(and x y z)
-          (and (= op 'not) (= (first (first (first args))) 'and)) (orexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (and x y))
-          (and (= op 'not) (= (first (first (first args))) 'or)) (andexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))  ;(not (or x y))
-          ;---- Own Simplifications
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) false            ;(and true false)
-          (and (and (and (and (= op 'and) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) false            ;(and false true)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (first (first args)) true) (= (second (first args)) false))))) true              ;(and true false)
-          (and (and (and (and (= op 'or) (= (count (first args)) 2) (= (second (first args)) true) (= (first (first args)) false))))) true              ;(and false true)
+          ;(or true)
+          (and
+            (= op 'or)
+            (= (count (first args)) 1)
+            (= (first args) '(true))) true
+
+          ;(or false)
+          (and
+            (= op 'or)
+            (= (count (first args)) 1)
+            (= (first args) '(false))) false
+
+          ;(and true)
+          (and
+            (= op 'and)
+            (= (count (first args)) 1)
+            (= (first args) '(true))) true
+
+          ;(and false)
+          (and
+            (= op 'and)
+            (= (count (first args)) 1)
+            (= (first args) '(false))) false
+
+          ;(or <const> false)
+          (and
+            (= op 'or)
+            (= (count (first args)) 2)
+            (not (= (first (first args)) true))
+            (= (second (first args)) false)) (first (first args))
+
+          ;(or false <const>)
+          (and
+            (= op 'or)
+            (= (count (first args)) 2)
+            (not (= (second (first args)) true))
+            (= (first (first args)) false)) (second (first args))
+
+          ;(or true <const>)
+          (and
+            (= op 'or)
+            (= (count (first args)) 2)
+            (= (first (first args)) true)) true
+
+          ;(or <const> true)
+          (and
+            (= op 'or)
+            (= (count (first args)) 2)
+            (= (second (first args)) true)) true
+
+          ;(and <const> false)
+          (and
+            (= op 'and)
+            (= (count (first args)) 2)
+            (= (second (first args)) false)) false
+
+          ;(and false <const>)
+          (and
+            (= op 'and)
+            (= (count (first args)) 2)
+            (= (first (first args)) false)) false
+
+          ;(and <const> true)
+          (and
+            (= op 'and)
+            (= (count (first args)) 2)
+            (= (second (first args)) true)) (first (first args))
+
+          ;(and true <const>)
+          (and
+            (= op 'and)
+            (= (count (first args)) 2)
+            (= (first (first args)) true)) (second (first args))
+
+          ;(not true)
+          (and
+            (= op 'not)
+            (= (count (first args)) 1)
+            (= (first args) '(true))) false
+
+          ;(not false)
+          (and
+            (= op 'not)
+            (= (count (first args)) 1)
+            (= (first args) '(false))) true
+
+          ;(or x y z)
+          (and
+            (= op 'or)
+            (= (count (first args)) 3)) (orexp (second (first args)) (nth (first args) 2) (first (first args)))
+
+          ;(and x y z)
+          (and
+            (= op 'and)
+            (= (count (first args)) 3)) (andexp (second (first args)) (nth (first args) 2) (first (first args)))
+
+          ;(not (and x y))
+          (and
+            (= op 'not)
+            (= (first (first (first args))) 'and)) (orexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))
+
+          ;(not (or x y))
+          (and
+            (= op 'not)
+            (= (first (first (first args))) 'or)) (andexp (notexp (second (first (first args)))) (notexp (nth (first (first args)) 2)))
           :else lst
       ))))
+
 
 ; Recursively applies subExpr to unevalueted boolean expression list
 (defn simplify [lst]
     (println "Going Down Recursively:\n***")
     (println lst)
       (cond
-        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (not (seq? (second lst)))) (not (seq? (nth lst 2)))) (subExpr lst)
-        (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 2)) (not (seq? (second lst)))) (subExpr lst)
-        (and (and (and (not (boolean? lst)) (seq? (first lst))) (= (count lst) 2)) (not (seq? (second lst)))) (subExpr (list (second lst) (simplify (first lst))))
-        (and (and (and (not (boolean? lst)) (seq? (second lst))) (= (count lst) 2)) (not (seq? (first lst)))) (subExpr (list (first lst) (simplify (second lst))))
-        (and (and (and (and (not (boolean? lst)) (seq? (first lst))) (and (= (count lst) 3))) (not (seq? (second lst)))) (not (seq? (nth lst 2)))) (subExpr (list (second lst) (nth lst 2) (simplify (first lst))))
-        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (seq? (second lst))) (not (seq? (nth lst 2)))) (subExpr (list (first lst) (simplify (second lst)) (nth lst 2)))
-        (and (and (and (and (not (boolean? lst)) (not (seq? (first lst)))) (= (count lst) 3)) (not (seq? (second lst)))) (seq? (nth lst 2))) (subExpr (list (first lst) (second lst) (simplify (nth lst 2))))
+        ; Base Case 1, Count == 3, no index is a list
+        (and
+          (not (boolean? lst))
+          (not (seq? (first lst)))
+          (= (count lst) 3)
+          (not (seq? (second lst)))
+          (not (seq? (nth lst 2)))) (subExpr lst)
+
+        ; Base Case 2, Count == 2, no index is a list
+        (and
+          (not (boolean? lst))
+          (not (seq? (first lst)))
+          (= (count lst) 2)
+          (not (seq? (second lst)))) (subExpr lst)
+
+        ; Recursively simplify first index if its a list and count == 2
+        (and
+          (not (boolean? lst))
+          (seq? (first lst))
+          (= (count lst) 2)
+          (not (seq? (second lst)))) (subExpr
+                                        (list
+                                          (second lst)
+                                          (simplify (first lst))))
+
+        ; Recursively simplify second index if its a list and count == 2
+        (and
+          (not (boolean? lst))
+          (seq? (second lst))
+          (= (count lst) 2)
+          (not (seq? (first lst)))) (subExpr
+                                      (list
+                                        (first lst)
+                                        (simplify (second lst))))
+
+        ; Recursively simplify first index if its a list and count == 3
+        (and
+          (not (boolean? lst))
+          (seq? (first lst))
+          (= (count lst) 3)
+          (not (seq? (second lst)))
+          (not (seq? (nth lst 2)))) (subExpr
+                                      (list
+                                        (second lst)
+                                        (nth lst 2)
+                                        (simplify (first lst))))
+
+        ; Recursively simplify second index if its a list and count == 3
+        (and
+          (not (boolean? lst))
+          (not (seq? (first lst)))
+          (= (count lst) 3)
+          (seq? (second lst))
+          (not (seq? (nth lst 2)))) (subExpr
+                                      (list
+                                        (first lst)
+                                        (simplify (second lst))
+                                        (nth lst 2)))
+
+        ; Recursively simplify third index if its a list and count == 3
+        (and
+          (not (boolean? lst))
+          (not (seq? (first lst)))
+          (= (count lst) 3)
+          (not (seq? (second lst)))
+          (seq? (nth lst 2))) (subExpr
+                                (list
+                                  (first lst)
+                                  (second lst)
+                                  (simplify (nth lst 2))))
+
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;; Added everything below after meeting with Prof. Schegel ;;
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        ; Recursively simplify first index is a list AND second index is a list and count == 3
+        (and
+          (not (boolean? lst))
+          (= (count lst) 3)
+          (seq? (first lst))
+          (seq? (second lst))
+          (not (seq? (nth lst 2)))) (subExpr
+                                      (list
+                                        (simplify (first lst))
+                                        (simplify (second lst))
+                                        (nth lst 2)))
+
+        ; Recursively simplify first index is a list AND third index is a list and count == 3
+        (and
+          (not (boolean? lst))
+          (= (count lst) 3)
+          (seq? (first lst))
+          (not (seq? (second lst)))
+          (seq? (nth lst 2))) (subExpr
+                                (list
+                                  (simplify (first lst))
+                                  (second lst)
+                                  (simplify (nth lst 2))))
+
+        ; Recursively simplify second index is a list AND third index is a list and count == 3
+        (and
+          (not (boolean? lst))
+          (= (count lst) 3)
+          (not (seq? (first lst)))
+          (seq? (second lst))
+          (seq? (nth lst 2))) (subExpr
+                                (list
+                                  (first lst)
+                                  (simplify (second lst))
+                                  (simplify (nth lst 2))))
+
+        ; Recursively simplify if all 3 indices are lists and count == 3
+        (and
+          (not (boolean? lst))
+          (= (count lst) 3)
+          (seq? (first lst))
+          (seq? (second lst))
+          (seq? (nth lst 2))) (subExpr
+                                (list
+                                  (simplify (first lst))
+                                  (simplify (second lst))
+                                  (simplify (nth lst 2))))
+
+          ; Recursively simplify if both indices are lists and count == 2
+          (and
+            (not (boolean? lst))
+            (= (count lst) 2)
+            (seq? (first lst))
+            (seq? (second lst))) (subExpr
+                                  (list
+                                    (simplify (first lst))
+                                    (simplify (second lst))))
+
+          ; if one of the first indices is false and op=and, expr=false
+          (and
+            (symbol? (first lst))
+            (= (first lst) 'and)
+            (or
+              (= (second lst) false)
+              (= (nth lst 2) false))) false
+
         :else lst
   ))
 
